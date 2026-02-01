cf4604e5c7220aa3604829cd970bf9d483cf41f2
zahid-digitalhumanities <zahidrana708@gmail.com>
2026-02-01 20:05:01 +0500

    Add basic workflow example script

---
+++ examples/basic_workflow.py
@@ -0,0 +1,7 @@
+"""
+basic_workflow.py
+First example script for Urdu Computational Philology Toolkit
+"""
+
+print("Urdu Computational Philology Toolkit - Basic Workflow")
+print("This script will demonstrate the research pipeline.")
